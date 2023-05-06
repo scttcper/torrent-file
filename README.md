68f42d831ebd494492c42dbb859659ac654372ef
Scott Cooper <scttcper@gmail.com>
2023-05-06 14:54:47 -0700

    fix: switch to github actions, bump dependencies (#74)
    * fix: switch to github actions, bump dependencies
    
    * update github action

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# torrent-file [![npm](https://badgen.net/npm/v/@ctrl/torrent-file)](https://www.npmjs.com/package/@ctrl/torrent-file) [![CircleCI](https://badgen.net/circleci/github/scttcper/torrent-file)](https://circleci.com/gh/scttcper/torrent-file) [![coverage](https://badgen.net/codecov/c/github/scttcper/torrent-file)](https://codecov.io/gh/scttcper/torrent-file) [![bundlesize](https://badgen.net/bundlephobia/min/@ctrl/torrent-file)](https://bundlephobia.com/result?p=@ctrl/torrent-file)
+# torrent-file [![npm](https://badgen.net/npm/v/@ctrl/torrent-file)](https://www.npmjs.com/package/@ctrl/torrent-file) [![coverage](https://badgen.net/codecov/c/github/scttcper/torrent-file)](https://codecov.io/gh/scttcper/torrent-file) [![bundlesize](https://badgen.net/bundlephobia/min/@ctrl/torrent-file)](https://bundlephobia.com/result?p=@ctrl/torrent-file)
 
 > Parse a torrent file and read encoded data. 
 
--- test/torrentFile.spec.ts
+++ test/torrentFile.spec.ts
@@ -1,27 +1,32 @@
-import fs from 'node:fs';
+import fs from 'node:fs/promises';
 import { URL } from 'node:url';
 
+// @ts-expect-error types are outdated
 import parseTorrent from 'parse-torrent';
 import { expect, it } from 'vitest';
 
 import { files, hash, info } from '../src/index.js';
 
-const file = new URL('./ubuntu-18.04.2-live-server-amd64.iso.torrent', import.meta.url);
+const filepath = new URL('./ubuntu-18.04.2-live-server-amd64.iso.torrent', import.meta.url);
 
 it('should have the same hash as parse-torrent', async () => {
-  expect(await hash(fs.readFileSync(file))).toBe(parseTorrent(fs.readFileSync(file)).infoHash);
+  const file = await fs.readFile(filepath);
+  expect(await hash(file)).toBe((await parseTorrent(file)).infoHash);
 });
-it('should have the same name as parse-torrent', () => {
-  expect(info(fs.readFileSync(file)).name).toEqual(parseTorrent(fs.readFileSync(file)).name);
+it('should have the same name as parse-torrent', async () => {
+  const file = await fs.readFile(filepath);
+  expect(info(file).name).toEqual((await parseTorrent(file)).name);
 });
-it('should parse files', () => {
-  expect(files(fs.readFileSync(file)).files[0]).toEqual({
+it('should parse files', async () => {
+  const file = await fs.readFile(filepath);
+  expect(files(file).files[0]).toEqual({
     length: 874512384,
     name: 'ubuntu-18.04.2-live-server-amd64.iso',
     offset: 0,
     path: 'ubuntu-18.04.2-live-server-amd64.iso',
   });
 });
-it('should parse file pieces', () => {
-  expect(files(fs.readFileSync(file)).pieces).toHaveLength(1668);
+it('should parse file pieces', async () => {
+  const file = await fs.readFile(filepath);
+  expect(files(file).pieces).toHaveLength(1668);
 });
