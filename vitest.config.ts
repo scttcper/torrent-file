c717dcb6859e32db8e82d93595427c6a497b7a93
Scott Cooper <scttcper@gmail.com>
2023-09-14 21:57:09 -0700

    feat: build for node 18
    BREAKING CHANGE: requires node >18

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,8 +4,8 @@ export default defineConfig({
   test: {
     threads: false,
     coverage: {
-      provider: 'c8',
       reporter: ['text', 'json', 'html'],
+      provider: 'v8',
     },
   },
 });
